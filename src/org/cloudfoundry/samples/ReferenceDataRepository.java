986e6bdf520acdee8aa75d606003e5fdbbb8c8be
tianxiaoliang <tianxiaoliang@caa3c39d-e56c-c142-89df-aeee41903936>
2012-04-16 12:04:04 +0000

    git-svn-id: https://svnserver.samsungcloud.org/svn/Example/tags/1.0/spring4mysql-master-slave@76 caa3c39d-e56c-c142-89df-aeee41903936

--- src/org/cloudfoundry/samples/ReferenceDataRepository.java
+++ src/org/cloudfoundry/samples/ReferenceDataRepository.java
@@ -37,24 +37,26 @@ public class ReferenceDataRepository {
 			 */
 			System.out.println("use the CloudPiServiceAPI");
 			List<DataSource> list = CloudpiService.getMysqlSources(type);
-			BasicDataSource ds = (BasicDataSource) list.get(new Random()
-					.nextInt(list.size()));
-			if (type.equals("master")) {
-				List<String> initSqls = new ArrayList<String>();
-				initSqls.add("create table if not exists current_states (id smallint primary key, state_code char(2), name varchar(50))");
-				initSqls.add("insert into current_states(id, state_code, name) values(1, 'MA', 'Massachusetts') ON DUPLICATE KEY UPDATE id=id");
-				initSqls.add("insert into current_states(id, state_code, name) values(2, 'NH', 'New Hampshire') ON DUPLICATE KEY UPDATE id=id");
-				initSqls.add("insert into current_states(id, state_code, name) values(3, 'ME', 'Maine') ON DUPLICATE KEY UPDATE id=id");
-				ds.setConnectionInitSqls(initSqls);
+			if (list.size() > 0) {
+				BasicDataSource ds = (BasicDataSource) list.get(new Random()
+						.nextInt(list.size()));
+				if (type.equals("master")) {
+					List<String> initSqls = new ArrayList<String>();
+					initSqls.add("create table if not exists current_states (id smallint primary key, state_code char(2), name varchar(50))");
+					initSqls.add("insert into current_states(id, state_code, name) values(1, 'MA', 'Massachusetts') ON DUPLICATE KEY UPDATE id=id");
+					initSqls.add("insert into current_states(id, state_code, name) values(2, 'NH', 'New Hampshire') ON DUPLICATE KEY UPDATE id=id");
+					initSqls.add("insert into current_states(id, state_code, name) values(3, 'ME', 'Maine') ON DUPLICATE KEY UPDATE id=id");
+					ds.setConnectionInitSqls(initSqls);
+				}
+				jdbcTemplate = new JdbcTemplate(ds);
 			}
-			jdbcTemplate = new JdbcTemplate(ds);
 		} else {
 			// here you can write your own method to get connections
 			System.out.println("use the VCAP_SERVICE ENV directly");
 			JSONArray jsonArray = null;
 			try {
-				 JSONObject rootObj = new JSONObject(
-				 System.getenv("VCAP_SERVICES"));
+				JSONObject rootObj = new JSONObject(
+						System.getenv("VCAP_SERVICES"));
 //				JSONObject rootObj = new JSONObject(
 //						"{\"mysql-5.1.410\":[{\"name\":\"swctest_115_mysqllvm-ubuntu-ebs_swctest\",\"label\":\"mysql-5.1.410\",\"plan\":\"free\",\"credentials\":[{\"username\":\"scalr\",\"host\":\"ec2-107-20-31-3.compute-1.amazonaws.com\",\"password\":\"TQbOTzWY98e82lWImiQR\",\"user\":\"scalr\",\"hostname\":\"ec2-107-20-31-3.compute-1.amazonaws.com\",\"type\":\"master\",\"port\":3306,\"name\":\"cloudpidb\"},{\"username\":\"scalr\",\"host\":\"ec2-23-22-31-206.compute-1.amazonaws.com\",\"password\":\"TQbOTzWY98e82lWImiQR\",\"user\":\"scalr\",\"hostname\":\"ec2-23-22-31-206.compute-1.amazonaws.com\",\"type\":\"slave\",\"port\":3306,\"name\":\"cloudpidb\"}]}]}");
 
@@ -77,27 +79,30 @@ public class ReferenceDataRepository {
 						list.add(connInfo);
 					}
 				}
-				Credential connInfo = list
-						.get(new Random().nextInt(list.size()));
-				try {
-					BasicDataSource ds = new BasicDataSource();
-					ds.setUrl("jdbc:mysql://" + connInfo.getHost() + ":"
-							+ connInfo.getPort() + "/" + connInfo.getDb());
-					ds.setDriverClassName("com.mysql.jdbc.Driver");
-					ds.setUsername(connInfo.getUsername());
-					ds.setPassword(connInfo.getPassword());
-					if (type.equals("master")) {
-						List<String> initSqls = new ArrayList<String>();
-						initSqls.add("create table if not exists current_states (id smallint primary key, state_code char(2), name varchar(50))");
-						initSqls.add("insert into current_states(id, state_code, name) values(1, 'MA', 'Massachusetts') ON DUPLICATE KEY UPDATE id=id");
-						initSqls.add("insert into current_states(id, state_code, name) values(2, 'NH', 'New Hampshire') ON DUPLICATE KEY UPDATE id=id");
-						initSqls.add("insert into current_states(id, state_code, name) values(3, 'ME', 'Maine') ON DUPLICATE KEY UPDATE id=id");
-						ds.setConnectionInitSqls(initSqls);
+				if (list.size() > 0) {
+					Credential connInfo = list.get(new Random().nextInt(list
+							.size()));
+					try {
+						BasicDataSource ds = new BasicDataSource();
+						ds.setUrl("jdbc:mysql://" + connInfo.getHost() + ":"
+								+ connInfo.getPort() + "/" + connInfo.getDb());
+						ds.setDriverClassName("com.mysql.jdbc.Driver");
+						ds.setUsername(connInfo.getUsername());
+						ds.setPassword(connInfo.getPassword());
+						if (type.equals("master")) {
+							List<String> initSqls = new ArrayList<String>();
+							initSqls.add("create table if not exists current_states (id smallint primary key, state_code char(2), name varchar(50))");
+							initSqls.add("insert into current_states(id, state_code, name) values(1, 'MA', 'Massachusetts') ON DUPLICATE KEY UPDATE id=id");
+							initSqls.add("insert into current_states(id, state_code, name) values(2, 'NH', 'New Hampshire') ON DUPLICATE KEY UPDATE id=id");
+							initSqls.add("insert into current_states(id, state_code, name) values(3, 'ME', 'Maine') ON DUPLICATE KEY UPDATE id=id");
+							ds.setConnectionInitSqls(initSqls);
+						}
+						jdbcTemplate = new JdbcTemplate(ds);
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-					jdbcTemplate = new JdbcTemplate(ds);
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
+
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
